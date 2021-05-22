23a802f4cdb8686ed14d78f143c7c1764d67f158
UltraViolet5 <70701763+UltraViolet5@users.noreply.github.com>
2021-05-22 13:34:02 +0200

    pierwszy commit

---
+++ OOPPA/OOPPA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOPPA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
